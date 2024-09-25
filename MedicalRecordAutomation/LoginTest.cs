189fedbb7009c6f3e696b99acda39240a7d558af
Balaji <balaji0017@gmail.com>
2024-09-25 09:51:22 +0530

    Added Invalid Login Test

--- MedicalRecordAutomation/LoginTest.cs
+++ MedicalRecordAutomation/LoginTest.cs
@@ -12,15 +12,27 @@ namespace ThomsonReuters.MedicalRecordAutomation
     public class LoginTest : AutomationWrapper
     {
         [Test]
-        public void ValidLoginTest()
+        [TestCase("admin", "pass", "OpenEMR")]
+        [TestCase("accountant", "accountant", "OpenEMR")]
+        public void ValidLoginTest(string username, string password, string expectedValue)
         {
-            driver.FindElement(By.Id("authUser")).SendKeys("admin");
-            driver.FindElement(By.CssSelector("#clearPass")).SendKeys("pass");
+            driver.FindElement(By.Id("authUser")).SendKeys(username);
+            driver.FindElement(By.CssSelector("#clearPass")).SendKeys(password);
             driver.FindElement(By.Id("login-button")).Click();
 
             //Assert the title  - OpenEMR
-            Assert.That(driver.Title, Is.EqualTo("OpenEMR"));
+            Assert.That(driver.Title, Is.EqualTo(expectedValue));
         }
 
+        [Test]
+        [TestCase("saul","saul234", "Invalid username or password")]
+        public void invalidLoginTest(string username,string password,string expectedError)
+        {
+            driver.FindElement(By.Id("authUser")).SendKeys(username);
+            driver.FindElement(By.CssSelector("#clearPass")).SendKeys(password);
+            driver.FindElement(By.Id("login-button")).Click();
+
+            //Assert the error - Invalid username or password
+        }
     }
 }
