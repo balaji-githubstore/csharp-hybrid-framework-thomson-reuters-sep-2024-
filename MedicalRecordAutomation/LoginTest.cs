10c9f6cb22350dd64f7ef0b9c6fb4f15e9b2a8f7
Balaji <balaji0017@gmail.com>
2024-09-25 09:35:03 +0530

    Added Base Class for Browser Config and Added ValidLoginTest

---
+++ MedicalRecordAutomation/LoginTest.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThomsonReuters.MedicalRecordAutomation.Base;
+
+namespace ThomsonReuters.MedicalRecordAutomation
+{
+    public class LoginTest : AutomationWrapper
+    {
+        [Test]
+        public void ValidLoginTest()
+        {
+            driver.FindElement(By.Id("authUser")).SendKeys("admin");
+            driver.FindElement(By.CssSelector("#clearPass")).SendKeys("pass");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            //Assert the title  - OpenEMR
+            Assert.That(driver.Title, Is.EqualTo("OpenEMR"));
+        }
+
+    }
+}
--- MedicalRecordAutomation/LoginUITest.cs
+++ MedicalRecordAutomation/LoginUITest.cs
@@ -5,33 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThomsonReuters.MedicalRecordAutomation.Base;
 
 namespace ThomsonReuters.MedicalRecordAutomation
 {
     /// <summary>
     /// Login UI Test Cases
     /// </summary>
-    public class LoginUITest
+    public class LoginUITest : AutomationWrapper
     {
-        IWebDriver driver;
-
-        [SetUp]
-        public void BeforeTestMethod()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
-            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
-        }
-
-        [TearDown]
-        public void AfterTestMethod()
-        {
-            driver.Dispose();
-        }
-
-
         [Test]
         public void TitleTest()
         {
