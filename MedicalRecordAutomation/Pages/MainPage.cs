73612df6237b9685dae2d6a1530751214439dafc
Balaji <balaji0017@gmail.com>
2024-09-26 10:58:29 +0530

    created page class

---
+++ MedicalRecordAutomation/Pages/MainPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThomsonReuters.MedicalRecordAutomation.Pages
+{
+    /// <summary>
+    /// All common elements like menu, profile which gets displayed on all pages 
+    /// will be handled here
+    /// </summary>
+    public class MainPage
+    {
+    }
+}
