6a60157db648a9323c8bc37293c5a56f18f16e6a
Balaji <balaji0017@gmail.com>
2024-09-26 09:42:17 +0530

    Created Code for GetSheetIntoObjectArray

--- MedicalRecordAutomation/ZDemo1Test.cs
+++ MedicalRecordAutomation/ZDemo1Test.cs
@@ -1,82 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ClosedXML.Excel;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using ClosedXML.Excel;
 
-namespace MedicalRecordAutomation
-{
-    /// <summary>
-    /// Will be deleted
-    /// </summary>
-    public class ZDemo1Test
-    {
+//namespace MedicalRecordAutomation
+//{
+//    /// <summary>
+//    /// Will be deleted
+//    /// </summary>
+//    public class ZDemo1Test
+//    {
 
-        public static object[] ValidData()
-        {
-            //testcase1 
-            //size - number of arguments
-            string[] dataSet1 = new string[2];
-            dataSet1[0] = "saul";
-            dataSet1[1] = "saul123";
+//        public static object[] ValidData()
+//        {
+//            //testcase1 
+//            //size - number of arguments
+//            string[] dataSet1 = new string[2];
+//            dataSet1[0] = "saul";
+//            dataSet1[1] = "saul123";
 
-            //testcase1
-            string[] dataSet2 = new string[2];
-            dataSet2[0] = "peter";
-            dataSet2[1] = "peter123";
+//            //testcase1
+//            string[] dataSet2 = new string[2];
+//            dataSet2[0] = "peter";
+//            dataSet2[1] = "peter123";
 
-            //testcase1
-            string[] dataSet3 = new string[2];
-            dataSet3[0] = "kim";
-            dataSet3[1] = "kim123";
+//            //testcase1
+//            string[] dataSet3 = new string[2];
+//            dataSet3[0] = "kim";
+//            dataSet3[1] = "kim123";
 
-            string[] dataSet4 = new string[2];
-            dataSet4[0] = "bala";
-            dataSet4[1] = "bala123";
+//            string[] dataSet4 = new string[2];
+//            dataSet4[0] = "bala";
+//            dataSet4[1] = "bala123";
 
-            //size - number of test case
-            object[] finalData = new object[4];
-            finalData[0] = dataSet1;
-            finalData[1] = dataSet2;
-            finalData[2] = dataSet3;
-            finalData[3] = dataSet4;
+//            //size - number of test case
+//            object[] finalData = new object[4];
+//            finalData[0] = dataSet1;
+//            finalData[1] = dataSet2;
+//            finalData[2] = dataSet3;
+//            finalData[3] = dataSet4;
 
-            return finalData;
+//            return finalData;
 
-        }
+//        }
 
-        [Test]
-        [TestCaseSource(nameof(ValidData))]
-        public void ValidTest(string username, string password)
-        {
-            Console.WriteLine("hello" + username + password);
-        }
-
-
-        /// <summary>
-        /// Will be deleted. Just for understanding excel read
-        /// </summary>
-        [Test]
-        public void ReadExcelTest()
-        {
-            var book = new XLWorkbook(@"C:\Mine\Company\Thomson Reuters Sep 2024\AutomationFrameworkSolution\MedicalRecordAutomation\TestData\openemr_data.xlsx");
-            var sheet = book.Worksheet("ValidLoginTest");
-            var range = sheet.RangeUsed();
-
-            for (int r = 2; r <= 3; r++)
-            {
-                for (int c = 1; c <= 3; c++)
-                {
-                    string value = range.Cell(r, c).GetString();
-                    Console.WriteLine(value);
-                }
-            }
-            //need to work - adding all the values to object[]
-
-            book.Dispose();
-        }
+//        [Test]
+//        [TestCaseSource(nameof(ValidData))]
+//        public void ValidTest(string username, string password)
+//        {
+//            Console.WriteLine("hello" + username + password);
+//        }
 
 
-    }
-}
+//        /// <summary>
+//        /// Will be deleted. Just for understanding excel read
+//        /// </summary>
+//        [Test]
+//        public void ReadExcelTest()
+//        {
+//            var book = new XLWorkbook(@"C:\Mine\Company\Thomson Reuters Sep 2024\AutomationFrameworkSolution\MedicalRecordAutomation\TestData\openemr_data.xlsx");
+//            var sheet = book.Worksheet("ValidLoginTest");
+//            var range = sheet.RangeUsed();
+//            int rowCount = range.RowCount();
+//            int cellCount=range.ColumnCount();
+
+//            //size - number of testcase
+//            object[] finalData = new object[rowCount-1];
+
+//            for (int r = 2; r <= rowCount; r++)
+//            {
+//                //size - number of arguments/columncount
+//                string[] dataSet = new string[cellCount];
+
+//                for (int c = 1; c <= cellCount; c++)
+//                {
+//                    string value = range.Cell(r, c).GetString();
+//                    Console.WriteLine(value);
+//                    dataSet[c-1]=value;
+//                }
+//                finalData[r-2] = dataSet;
+//            }
+//            book.Dispose();
+//        }
+//    }
+//}
